d228dba27283c6d06de3f161a85ed42be4fc2f90
Krystof Rainisch <Krystof.Rainisch@hey.com>
2022-07-05 18:28:45 +0200

    add level logic

--- src/Models/Level/Level.ts
+++ src/Models/Level/Level.ts
@@ -1,6 +1,24 @@
 import type Tile from '../Tile'
+import type Entity from '../Entity'
+
+interface ChildLevel {
+  x: number
+  y: number
+  w: number
+  h: number
+  level: Level
+}
+
+interface TileContent {
+  tile: Tile
+  entities: Entity[]
+  x: number
+  y: number
+}
 
 export default class Level {
+  entities: Entity[] = []
+  children: ChildLevel[] = []
   constructor(
     public width: number,
     public height: number,
@@ -11,6 +29,69 @@ export default class Level {
     }
     if (height < 1) {
       throw new Error('height must be greater than 0')
+    }
+  }
+
+  public getTile(x: number, y: number): Tile {
+    if (x < 0 || x >= this.width) {
+      throw new Error('x must be between 0 and width')
+    }
+    if (y < 0 || y >= this.height) {
+      throw new Error('y must be between 0 and height')
+    }
+    return this.tiles[y * this.width + x]
+  }
+
+  public getChildAt(x: number, y: number): ChildLevel | undefined {
+    for (const child of this.children) {
+      if (
+        x >= child.x &&
+        x < child.x + child.w &&
+        y >= child.y &&
+        y < child.y + child.h
+      ) {
+        return child
+      }
+    }
+    return undefined
+  }
+
+  public addChild(x: number, y: number, level: Level): void {
+    const width = level.width
+    const height = level.height
+    // x must be >= 0 and x + width must be <= this.width
+    if (x < 0 || x + width > this.width) {
+      throw new Error('x must be between 0 and width')
+    }
+    // y must be >= 0 and y + height must be <= this.height
+    if (y < 0 || y + height > this.height) {
+      throw new Error('y must be between 0 and height')
+    }
+    this.children.push({
+      x,
+      y,
+      w: width,
+      h: height,
+      level,
+    })
+  }
+
+  public getTileContent(x: number, y: number): TileContent | undefined {
+    const child = this.getChildAt(x, y)
+    if (child) {
+      return child.level.getTileContent(x - child.x, y - child.y)
+    }
+    // entities
+    const tile = this.getTile(x, y)
+
+    const entities: Entity[] = this.entities.filter(
+      (entity) => entity.x === x && entity.y === y
+    )
+    return {
+      tile,
+      entities,
+      x,
+      y,
     }
   }
 }
